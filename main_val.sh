1e55ddf50a0c1276c4f9bc8a8939239e95cfb743
Kuang <haofeik@f8ffc22b1e09.ant.amazon.com>
2021-02-04 18:01:07 +0800

    update dir of k400

--- main_val.sh
+++ main_val.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-data_dir="/dataset/Kinetics_t30_s256"
+data_dir="/home/ubuntu/drive2/kinetics400_30fps_frames"
 output_dir="./model"
 eval_dir="./model/eval_svm"
 pretrained="pretrain/moco_v2_200ep_pretrain.pth.tar"
@@ -12,21 +12,21 @@ mkdir -p ${eval_dir}
 python3 -m torch.distributed.launch --master_port 9999 --nproc_per_node=${num_replica} \
     eval_svm_feature_extract.py \
     --data_dir=${data_dir} \
-    --datasplit=train_list \
+    --datasplit=train \
     --pretrained_model=${output_dir}/current.pth \
     --output_dir=${eval_dir}
 
 python3 -m torch.distributed.launch --master_port 9999 --nproc_per_node=${num_replica} \
     eval_svm_feature_extract.py \
     --data_dir=${data_dir} \
-    --datasplit=val_list \
+    --datasplit=val \
     --pretrained_model=${output_dir}/current.pth \
     --output_dir=${eval_dir}
 
 
 python3 eval_svm_feature_perf.py \
     --trainsplit=train_list \
-    --valsplit=val_list \
+    --valsplit=val \
     --output-dir=${eval_dir} \
     --num_replica=${num_replica} \
     --primal
